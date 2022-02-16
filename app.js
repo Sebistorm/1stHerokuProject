8b3a5aea906ad0807252a73cdc7cebcc2fae8018
LAPTOP-4141SHPJ\seb99 <seb994@live.dk>
2022-02-16 17:56:01 +0100

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 8080
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 })
 
 
-app.listen(8080, () => {
-    console.log("The server is running on Port: ", 8080)
+app.listen(port, () => {
+    console.log("The server is running on Port: ", port)
 })
